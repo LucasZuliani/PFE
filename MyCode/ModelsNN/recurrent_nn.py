67af28fee085859fb0c771eaf191e68c5f78103b
Lucas <lucas.zuliani.toulouse@gmail.com>
2024-10-16 22:15:06 +0200

    Some doctest and I've changed the definition of MSELoss using a class

--- MyCode/ModelsNN/recurrent_nn.py
+++ MyCode/ModelsNN/recurrent_nn.py
@@ -1,15 +1,24 @@
-import torch 
+import torch
 
 class ActivationCube(torch.nn.Module):
+    """
+    Activation function that returns the cube of the input if it is positive, and 0 otherwise, used in the paper for Deep Ritz method.
+
+    >>> activation = ActivationCube()
+    >>> activation(torch.tensor(2.))
+    tensor(8.)
+    >>> activation(torch.tensor(-2.))
+    tensor(0.)
+    """
     def __init__(self) -> None:
         super(ActivationCube, self).__init__()
 
     def forward(self, x):
         return torch.maximum(x**3, torch.tensor(0.))
 
-class RecurentBlock(torch.nn.Module):
+class RecurrentBlock(torch.nn.Module):
     def __init__(self, hidden_size):
-        super(RecurentBlock, self).__init__()
+        super(RecurrentBlock, self).__init__()
         self.fc1 = torch.nn.Linear(in_features=hidden_size, out_features=hidden_size)
         self.activation1 = ActivationCube()
         self.fc2 = torch.nn.Linear(in_features=hidden_size, out_features=hidden_size)
@@ -25,17 +34,18 @@ class RecurentBlock(torch.nn.Module):
         return x
 
 class RitzModel(torch.nn.Module):
-
     def __init__(self, num_blocks=4, hidden_size=10):
         super(RitzModel, self).__init__()
 
         self.fc_in = torch.nn.Linear(1, hidden_size)
-        self.blocks = torch.nn.ModuleList([RecurentBlock(hidden_size=hidden_size) for _ in range(num_blocks)])
+        self.blocks = torch.nn.ModuleList([RecurrentBlock(hidden_size=hidden_size) for _ in range(num_blocks)])
         self.fc_out = torch.nn.Linear(hidden_size, 1)
 
         for m in self.modules():
             if isinstance(m, torch.nn.Linear):
                 torch.nn.init.normal_(m.weight, mean=0, std=0.1)
+
+        self.nb_params = sum(p.numel() for p in self.parameters() if p.requires_grad)
 
     def forward(self, x):
         
