8e7a989018c12a5f190a44fbf822c9a7ef4cf7be
Lucas <lucas.zuliani.toulouse@gmail.com>
2024-11-07 00:19:24 +0100

    Saving the figures now

--- MyCode/Ex2_singularity2D/ex2_plots.py
+++ MyCode/Ex2_singularity2D/ex2_plots.py
@@ -28,13 +28,20 @@ def partial_derivative(u, x):
     grad_ux1, grad_ux2 = grad_u[:, 0], grad_u[:, 1]
     return grad_ux1, grad_ux2
 
-def config_one_plot(axes, fig, ind, to_plot, title):
-    im = axes[ind].imshow(to_plot, cmap='jet', extent=[-1, 1, -1, 1], interpolation='bicubic')
-    axes[ind].set_title(title)
-    fig.colorbar(im, ax=axes[ind], shrink=0.6)
+def config_one_plot_im(axes, fig, inds, to_plot, title):
+    if isinstance(inds, int):
+        ind = inds
+        im = axes[ind].imshow(to_plot, cmap='jet', extent=[-1, 1, -1, 1], interpolation='bicubic')
+        axes[ind].set_title(title)
+        fig.colorbar(im, ax=axes[ind], shrink=0.6)
+    else:
+        ind_i, ind_j = inds
+        im = axes[ind_i, ind_j].imshow(to_plot, cmap='jet', extent=[-1, 1, -1, 1], interpolation='bicubic')
+        axes[ind_i, ind_j].set_title(title)
+        fig.colorbar(im, ax=axes[ind_i, ind_j], shrink=0.6)
     return 
 
-def assess_solution(model):
+def assess_solution(model, model_name):
     evaluation_domain = Corner_Singularity_2D()
     squared_grid_size = evaluation_domain.squared_grid_size
     evaluation_domain_points = torch.FloatTensor(evaluation_domain.uniform_all_points)
@@ -72,29 +79,36 @@ def assess_solution(model):
     print(f"Norm H1 of the relative error: {err_relative_h1}")
 
     fig0, axes0 = plt.subplots(1, 3, figsize=(12, 5))
-    config_one_plot(axes0, fig0, 0, u_exact, 'Exact solution')
-    config_one_plot(axes0, fig0, 1, u_pred, 'Predicted solution')
-    config_one_plot(axes0, fig0, 2, diff_abs, 'Absolute difference')
+    config_one_plot_im(axes0, fig0, 0, u_exact, 'Exact solution')
+    config_one_plot_im(axes0, fig0, 1, u_pred, 'Predicted solution')
+    config_one_plot_im(axes0, fig0, 2, diff_abs, 'Absolute difference')
+    fig0.savefig(f'./Ex2_singularity2D/Figures/{model_name}_U.png')
     plt.tight_layout()
     plt.show()
 
     fig1, axes1 = plt.subplots(1, 3, figsize=(12, 5))
-    config_one_plot(axes1, fig1, 0, du_exact_x1, 'Exact du/dx1')
-    config_one_plot(axes1, fig1, 1, du_pred_x1, 'Predicted du/dx1')
-    config_one_plot(axes1, fig1, 2, diff_abs_dx1, 'Absolute difference du/dx1')
+    config_one_plot_im(axes1, fig1, 0, du_exact_x1, 'Exact du/dx1')
+    config_one_plot_im(axes1, fig1, 1, du_pred_x1, 'Predicted du/dx1')
+    config_one_plot_im(axes1, fig1, 2, diff_abs_dx1, 'Absolute difference du/dx1')
     plt.tight_layout()
     plt.show()
 
     fig2, axes2 = plt.subplots(1, 3, figsize=(12, 5))
-    config_one_plot(axes2, fig2, 0, du_exact_x2, 'Exact du/dx2')
-    config_one_plot(axes2, fig2, 1, du_pred_x2, 'Predicted du/dx2')
-    config_one_plot(axes2, fig2, 2, diff_abs_dx2, 'Absolute difference du/dx2')
+    config_one_plot_im(axes2, fig2, 0, du_exact_x2, 'Exact du/dx2')
+    config_one_plot_im(axes2, fig2, 1, du_pred_x2, 'Predicted du/dx2')
+    config_one_plot_im(axes2, fig2, 2, diff_abs_dx2, 'Absolute difference du/dx2')
     plt.tight_layout()
     plt.show()
+
+    # fig3, axes3 = plt.subplots(2, 2, figsize=(12, 5))
+    # axes3[0, 0].plot(evaluation_domain_points[:20, 0].detach().numpy(), tensor_du_exact_x1[:20].detach().numpy(), label='Exact du/dx1')
+    # axes3[0, 1].plot(evaluation_domain_points[:20, 0].detach().numpy(), tensor_du_pred_x1[:20].detach().numpy(), label='Predicted du/dx1')
+    # plt.tight_layout()
+    # plt.show()
     
 if __name__=="__main__":
-    model_name = 'ex2_res_iter20k_10k_2500_beta500.pth'
+    model_name = 'ex2_res_iter20k_10k_2500_beta500'
     model = rnn.RitzModel(2).to(('cpu'))
-    model.load_state_dict(torch.load('./Ex2_singularity2D/Models/'+model_name, weights_only=True))
+    model.load_state_dict(torch.load('./Ex2_singularity2D/Models/'+model_name+'.pth', weights_only=True))
     print('\n')
-    assess_solution(model)
+    assess_solution(model, model_name)
