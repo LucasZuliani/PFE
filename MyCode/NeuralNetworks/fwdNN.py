2082e7524bcd5b82bb44bfd7a55e44f018553e02
Lucas <lucas.zuliani.toulouse@gmail.com>
2024-11-27 21:06:13 +0100

    We can now chose the number of hidden layers for the fwd network

--- MyCode/NeuralNetworks/fwdNN.py
+++ MyCode/NeuralNetworks/fwdNN.py
@@ -1,12 +1,13 @@
 import torch
 
 class FullyConnectedNetwork(torch.nn.Module):
-    def __init__(self, hidden_size=20, input_dim=2):
+    def __init__(self, hidden_size=20, n_hidden_layers=2, input_dim=2):
         super(FullyConnectedNetwork, self).__init__()
 
         self.fc_in = torch.nn.Linear(in_features=input_dim, out_features=hidden_size)
-        self.fc2 = torch.nn.Linear(in_features=hidden_size, out_features=hidden_size)
-        self.fc3 = torch.nn.Linear(in_features=hidden_size, out_features=hidden_size)
+        self.hidden_layers = torch.nn.ModuleList(
+            [torch.nn.Linear(in_features=hidden_size, out_features=hidden_size) for _ in range(n_hidden_layers)]
+        )
         self.fc_out = torch.nn.Linear(in_features=hidden_size, out_features=1)
 
         self.activation = torch.nn.Tanh()
@@ -19,8 +20,10 @@ class FullyConnectedNetwork(torch.nn.Module):
 
     def forward(self, x):
         x = self.activation(self.fc_in(x))
-        x = self.activation(self.fc2(x))
-        x = self.activation(self.fc3(x))
+
+        for layer in self.hidden_layers:
+            x = self.activation(layer(x))
+
         u_theta = self.fc_out(x)
 
         return u_theta
